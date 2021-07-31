efd687f38b95b4c04b128cb5d552a0e29c88057b
Abhisekh Shaw <abhishekshaw1007@gmail.com>
2021-08-01 01:12:10 +0530

    final code changes

--- src/components/circle.js
+++ src/components/circle.js
@@ -2,6 +2,15 @@
 
 // declare class
 
-
 // export class using module.exports
 // new commit
+import Shape from "./Shape";
+export default class Circle extends Shape {
+  constructor() {
+    super();
+  }
+  calculateArea() {
+    console.log("area of circle is: ");
+    return 3.14 * 5 * 5;
+  }
+}
--- src/components/shape.js
+++ src/components/shape.js
@@ -2,3 +2,16 @@
 
 // export class using module.exports
 // new commit
+
+export default class Shape {
+  constructor() {
+    this.color = "red";
+  }
+  drawShape() {
+    console.log("shape is the required shape");
+  }
+  calculateArea() {
+    console.log("don't know the area of unknown shape");
+    return 0;
+  }
+}
